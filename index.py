c67437529465b3f14d985c2f8b3e41338c7a328e
masde445 <student@ubuntu-20.edu-wsb.local>
2023-05-07 11:30:20 +0200

    GET

--- index.py
+++ index.py
@@ -2,7 +2,11 @@ import requests
 
 def pobierz(sciezka)
     dane = requests.get(sciezka).text
-    return dane
+    if dane.status_code > 299:
+        return 0
+    else:
+        dane = dane.text
+        return dane
 
 xd = pobierz('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
 print(xd)
