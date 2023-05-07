7fb366805380969c20a6c86330eca654b16fbab9
masde445 <student@ubuntu-20.edu-wsb.local>
2023-05-07 11:57:30 +0200

    v2

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def pobierz(sciezka)
     dane = requests.get(sciezka).text
     if dane.status_code > 299:
@@ -8,5 +9,12 @@ def pobierz(sciezka)
         dane = dane.text
         return dane
 
+def post(sciezka)
+    x = {"name":"Natalia"}
+    dane = requests.post(sciezka, json=x)
+    return dane.text
+
 xd = pobierz('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
 print(xd)
+xd2 = post('https://httpbin.org/post')
+print(xd2)
